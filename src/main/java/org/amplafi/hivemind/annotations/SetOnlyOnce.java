65d0a74250ea41df4fef25ed43de36f8f4399d08
patmoore <patmoore@amplafi.com>
2009-03-04 19:54:57 +0000

    corrected copyright notice
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-hivemind/trunk@195 6eaf30a2-012b-4626-ba8e-69dab18af4f2

--- src/main/java/org/amplafi/hivemind/annotations/SetOnlyOnce.java
+++ src/main/java/org/amplafi/hivemind/annotations/SetOnlyOnce.java
@@ -1,7 +1,17 @@
 /*
- * Created on Sep 15, 2006
- * Copyright 2006 by Patrick Moore
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
  */
+
 package org.amplafi.hivemind.annotations;
 
 import java.lang.annotation.Retention;
@@ -11,8 +21,8 @@ import java.lang.annotation.ElementType;
 /**
  * use to flag a method or field that should be
  * set only once. It is ok to set the value to the same value.
- * 
- * If a method is flagged, it must be the setter and the 
+ *
+ * If a method is flagged, it must be the setter and the
  * field must follow the bean pattern.
  */
 @Target({ElementType.FIELD, ElementType.METHOD})
